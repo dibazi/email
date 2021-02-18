e503fa17066e9db251f6d57ca6ac8585cb1b1ba7
dibazi <bertod48@yahoo.fr>
2021-02-18 22:35:41 +0200

    email3

--- contact.php
+++ contact.php
@@ -13,6 +13,6 @@ if(isset($_POST['submit'])){
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
     mail($mailTo,$subject, $txt, $header);
-    header("Location: index.php");
+    header("Location: email/index.php");
 }
 
