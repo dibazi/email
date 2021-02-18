999d15ec724c8388994393de2819affd8095383f
dibazi <bertod48@yahoo.fr>
2021-02-18 22:20:23 +0200

    email1

--- contact.php
+++ contact.php
@@ -2,16 +2,17 @@
 
 if(isset($_POST['submit'])){
 
+    
     $name = $_POST['name'];
+    $subject = $_POST['subject'];
     $emailfrom = $_POST['email'];
     $message = $_POST['message'];
 
-    $maitTo = "info@senga-service.com";
-    $header = "From: ".$mailFrom;
+    $mailTo = "info@senga-service.com";
+    $header = "From: ".$emailfrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-    mail($maitTo, $txt, $header);
-    header("Location: index.php?mailsend");
+    mail($mailTo,$subject, $txt, $header);
+    header("Location: index.php");
 }
 
-?>
